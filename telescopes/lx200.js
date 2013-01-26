bcb1d3d468b5ddf276677347aec37138a2baf489
Ferran Basora <fcsonline@gmail.com>
2013-01-26 19:13:57 +0100

    New LX200 server

---
+++ telescopes/lx200.js
@@ -0,0 +1,15 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var http = require('http');
+var microtime = require('microtime');
+var utils = require('../utils');
+
+function Telescope() {
+  this.move = function (position) {
+    console.log("Moving LX200 telescope to: ", position);
+  };
+}
+
+util.inherits(Telescope, EventEmitter);
+
+module.exports = Telescope;
