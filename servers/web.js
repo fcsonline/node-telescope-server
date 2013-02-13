bf3d71b11e3feb57c9c314f949d9f942b867f650
Ferran Basora <fcsonline@gmail.com>
2013-02-13 09:48:17 +0100

    New express api endpoint

--- servers/web.js
+++ servers/web.js
@@ -1,12 +1,35 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
-var http = require('http');
+var express = require('express');
 var microtime = require('microtime');
 var utils = require('../utils');
 
 function Server(params) {
+  var self = this;
+
   this.listen = function () {
     console.log(utils.welcome(params));
+
+    var app = express.createServer();
+
+    app.post('/api/position', function (req, res) {
+      var ra = req.param('ra', null)
+        , dec = req.param('dec', null);
+
+      if (!ra || !dec) {
+        res.send("Missing ra or dec parameters", 400);
+        return;
+      }
+
+      self.emit('move', {
+        ra: ra
+      , dec: dec
+      });
+
+      res.send(200);
+    });
+
+    app.listen(params.port);
   };
 }
 
