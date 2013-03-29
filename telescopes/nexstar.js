6e7a5326ab053354879d9193ef333e60c3bda008
Ferran Basora <fcsonline@gmail.com>
2013-03-29 01:51:02 +0100

    Added the communicaction with nexstar telescopes

--- telescopes/nexstar.js
+++ telescopes/nexstar.js
@@ -4,8 +4,15 @@ var EventEmitter = require('events').EventEmitter;
 var http = require('http');
 var microtime = require('microtime');
 var utils = require('../utils');
+var ExponentialBuffer = require('../lib/exponential.buffers');
 
 function Telescope(params) {
+
+  fs.exists(params.telescopeDevice, function (exists) {
+    if (!exists) {
+      throw Error('Unable to open the NexStar device descriptior: ' + params.telescopeDevice);
+    }
+  });
 
   // Stream to telescope device
   this.device = fs.createWriteStream(params.telescopeDevice, {'flags': 'a'});
@@ -13,16 +20,24 @@ function Telescope(params) {
   // Command queue
   this.queue = [];
 
+  // Session closed
   this.device.on("end", function () {
     console.log("Nextar telescope disconected!");
   });
 
+  // Incomming data
   this.device.on("data", function (data) {
-    var ibuffer = new Buffer(data)
+    var ibuffer = new ExponentialBuffer(data)
       , ra_int
       , dec_int;
 
-    // TODO READ: <ra 8 bytes>,<dec 8 bytes>#
+    if (params.debug) {
+      console.log('Nexstar input: ', ibuffer);
+    }
+
+    // READ: <ra 8 bytes>,<dec 8 bytes>#
+    ra_int = ibuffer.readDoubleExponential(0);
+    dec_int = ibuffer.readDoubleExponential(9);
 
     this.emit('track', {
       ra: ra_int
@@ -30,7 +45,7 @@ function Telescope(params) {
     });
   });
 
-
+  // Outgoing data
   this.goto = function (position) {
 
     if (!params.quiet) {
@@ -44,9 +59,19 @@ function Telescope(params) {
 
   this.process = function () {
     var command = this.queue.shift()
-      , obuffer = new Buffer(18);
+      , obuffer = new ExponentialBuffer(18);
 
-    // TODO WRITE: r<ra 8 bytes>,<dec 8 bytes>
+    console.log("Command: ", command);
+
+    // WRITE: r<ra 8 bytes>,<dec 8 bytes>
+    obuffer.write('r', 0);
+    obuffer.writeDoubleExponential(command.ra, 1);
+    obuffer.write(',', 9);
+    obuffer.writeDoubleExponential(command.dec, 10);
+
+    if (params.debug) {
+      console.log('Nexstar output: ', obuffer);
+    }
 
     // Write the next command and wait for a device response
     this.device.write(obuffer);
